03f18e469a6c8fc35ec5ee7cd1359c75db8c2087
asherikov <asherikov@yandex.com>
2018-07-19 16:22:03 +0300

    Add qpmadConfig.cmake

--- tests/utf_common.h
+++ tests/utf_common.h
@@ -11,6 +11,7 @@
 #define BOOST_TEST_MODULE qpmad
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+#include <boost/test/results_reporter.hpp>
 #include <boost/timer/timer.hpp>
 #include <boost/lexical_cast.hpp>
 
@@ -27,7 +28,8 @@ struct GlobalFixtureConfig
 {
     GlobalFixtureConfig()
     {
-        boost::unit_test::unit_test_log.set_threshold_level( boost::unit_test::log_successful_tests );
+        //boost::unit_test::unit_test_log.set_threshold_level( boost::unit_test::log_successful_tests );
+        boost::unit_test::results_reporter::set_level( boost::unit_test::DETAILED_REPORT );
     }
     ~GlobalFixtureConfig() {}
 };
