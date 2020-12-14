181b4eb89a636469937ceab75956f03b17edb4c7
asherikov <asherikov@yandex.com>
2020-12-14 20:57:45 +0100

    CI: add scan build test

--- .make/common.mk
+++ .make/common.mk
@@ -44,3 +44,62 @@ spell:
 
 format:
 	${FIND_QPMAD_SOURCES} | grep -v "cpput" | grep -v "eigenut" | xargs clang-format80 -verbose -i
+
+
+clangcheck:
+	${SCANBUILD} \
+        -o ./scanbuild_results \
+        --status-bugs \
+        --exclude ./build \
+        --exclude /usr/include/ \
+        --exclude /usr/local/include/ \
+        --exclude /usr/src/ \
+        --exclude /opt/ros/ \
+        -enable-checker core.CallAndMessage \
+        -enable-checker core.DivideZero \
+        -enable-checker core.DynamicTypePropagation \
+        -enable-checker core.NonNullParamChecker \
+        -enable-checker core.NullDereference \
+        -enable-checker core.StackAddressEscape \
+        -enable-checker core.UndefinedBinaryOperatorResult \
+        -enable-checker core.VLASize \
+        -enable-checker core.uninitialized.ArraySubscript \
+        -enable-checker core.uninitialized.Assign \
+        -enable-checker core.uninitialized.Branch \
+        -enable-checker core.uninitialized.CapturedBlockVariable \
+        -enable-checker core.uninitialized.UndefReturn \
+        -enable-checker cplusplus.InnerPointer \
+        -enable-checker cplusplus.Move \
+        -enable-checker cplusplus.NewDelete \
+        -enable-checker cplusplus.NewDeleteLeaks \
+        -enable-checker deadcode.DeadStores \
+        -enable-checker nullability.NullPassedToNonnull \
+        -enable-checker nullability.NullReturnedFromNonnull \
+        -enable-checker nullability.NullableDereferenced \
+        -enable-checker nullability.NullablePassedToNonnull \
+        -enable-checker nullability.NullableReturnedFromNonnull \
+        -enable-checker optin.cplusplus.UninitializedObject \
+        -enable-checker optin.mpi.MPI-Checker \
+        -enable-checker optin.performance.GCDAntipattern \
+        -enable-checker optin.performance.Padding \
+        -enable-checker optin.portability.UnixAPI \
+        -enable-checker security.FloatLoopCounter \
+        -enable-checker security.insecureAPI.DeprecatedOrUnsafeBufferHandling \
+        -enable-checker security.insecureAPI.UncheckedReturn \
+        -enable-checker security.insecureAPI.getpw \
+        -enable-checker security.insecureAPI.gets \
+        -enable-checker security.insecureAPI.mkstemp \
+        -enable-checker security.insecureAPI.mktemp \
+        -enable-checker security.insecureAPI.vfork \
+        -enable-checker unix.API \
+        -enable-checker unix.Malloc \
+        -enable-checker unix.MallocSizeof \
+        -enable-checker unix.MismatchedDeallocator \
+        -enable-checker unix.Vfork \
+        -enable-checker unix.cstring.BadSizeArg \
+        -enable-checker unix.cstring.NullArg \
+        -enable-checker valist.CopyToSelf \
+        -enable-checker valist.Uninitialized \
+        -enable-checker valist.Unterminated \
+        ${MAKE} test
+
